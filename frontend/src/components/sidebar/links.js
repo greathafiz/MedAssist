1e9552d1674044c544a0219e7f03c31b2401bdab
Oyedare <ooyedaretemiloluwa@gmail.com>
2023-11-23 11:16:02 +0100

    Create sidebar

---
+++ frontend/src/components/sidebar/links.js
@@ -0,0 +1,31 @@
+import DashboardIcon from '../../assets/dashboard-icon.svg'
+import MedicationIcon from '../../assets/medication-icon.svg'
+import CalendarIcon from '../../assets/calendar-icon.svg'
+import ReportIcon from '../../assets/report-icon.svg'
+
+export const links = [
+    {
+        id: 1,
+        name: "Dashboard",
+        icon: DashboardIcon,
+        href: '/'
+    },
+    {
+        id: 2,
+        name: "Medications",
+        icon: MedicationIcon,
+        href: '/medications'
+    },
+    {
+        id: 3,
+        name: "Calendar",
+        icon: CalendarIcon,
+        href: '/calendar'
+    },
+    {
+        id: 4,
+        name: "Report",
+        icon: ReportIcon,
+        href: '/report'
+    },
+]
